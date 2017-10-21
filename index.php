3585966babd414fe8b3abe749d7c07083e8fbcde
Joshua Jimenez <jimenez.johnjoshua.jjj@gmail.com>
2017-10-21 22:32:41 +0800

    Migrated stylesheet links as script files for reusability.

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
         <title>Login - OCES</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="resources/css/bootstrap.css">
-        <link rel="stylesheet" href="resources/css/style.css">
+        <?php
+            require 'partials/stylesheets.php';
+        ?>
     </head>
 
     <body>
@@ -30,7 +31,7 @@
         </div>
     </body>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="resources/js/bootstrap.min.js" ></script>
+    <?php
+        require ('partials/scripts.php');
+    ?>
 </html>
