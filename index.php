cee0ebd7255d21c6f3f6154ff06947f52f38e7cc
Joshua Jimenez <jimenez.johnjoshua.jjj@gmail.com>
2017-12-03 21:22:01 +0800

    Merged approval and viewing of reports, closes #2 and closes #3.

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   </head>
 
+    
   <body>
   <!-- HEADING -->
   <div style="position: fixed; width: 100%;">
